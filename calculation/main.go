8cbea9dadb5c8a4b2bcc08cf27536608f90beac4
lamechang-van <yoshimoto2107@gmail.com>
2023-02-26 16:22:36 +0900

    :sparkles:add calculation.go

---
+++ calculation/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nn = 100 + 200
+
+	// 整数の演算
+	n := 100 + 200
+	// 変数を使った演算
+	m := n + 100
+	// 文字列の足し算
+	msg := "hoge" + "fuga"
+
+	fmt.Println(n, nn, m, msg)
+}
