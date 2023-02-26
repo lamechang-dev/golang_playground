c863f38a28df9af98ff22950a4dc06962dd19a5c
lamechang-van <yoshimoto2107@gmail.com>
2023-02-26 16:52:05 +0900

    :sparkles:

---
+++ struct/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	p := struct {
+		name   string
+		age    int
+		height float32
+	}{
+		name:   "John",
+		age:    12,
+		height: 172.2,
+	}
+
+	fmt.Println(p)
+}
