b0d027f028ba729bd2ed1ebc9a95570e215f7c61
Ryosuke <yoshimoto2107@gmail.com>
2024-06-08 21:27:58 +0900

    :sparkles:

--- array/main.go
+++ array/main.go
@@ -3,24 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	ns := []int{1, 2, 3, 4, 5}
-	fmt.Println(ns)
-	slicedNs := ns[:3]
-	fmt.Println(slicedNs)
-	fmt.Println(len(slicedNs))
-	fmt.Println(cap(slicedNs))
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
 
-	println(len(ns), cap(ns))
-	// 容量が足りない場合
-	// 元のおよそ2倍の容量の配列を確保しなおす
-	ns = append(ns, 6, 7)
-	fmt.Println(ns)
-	println(len(ns), cap(ns))
-
-	println("==============")
-
-	// range の例
-	for _, v := range ns {
-		fmt.Println(v)
-	}
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
 }
